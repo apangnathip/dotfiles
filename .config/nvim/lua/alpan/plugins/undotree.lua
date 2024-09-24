447fb5674cb1884729d15c3f6d19a9e7842361fb
apangnathip <alanpangnathip@gmail.com>
2024-09-24 00:05:31 -0400

    make undotree focus when popup

--- .config/nvim/lua/alpan/plugins/undotree.lua
+++ .config/nvim/lua/alpan/plugins/undotree.lua
@@ -3,5 +3,6 @@ return {
 	event = { "BufReadPre", "BufNewFile" },
 	config = function()
 		vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle, { desc = "Undotree" })
+		vim.g.undotree_SetFocusWhenToggle = 1
 	end,
 }
