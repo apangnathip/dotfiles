abe63e4df9f0d2f97ea29e080a511664b1c1887b
apangnathip <alanpangnathip@gmail.com>
2024-10-07 17:39:03 -0400

    change look of status column

--- .config/nvim/lua/alpan/core/set.lua
+++ .config/nvim/lua/alpan/core/set.lua
@@ -4,9 +4,12 @@ vim.g.lisp_rainbow = 1
 vim.opt.guicursor = "n-v-c:block-bCursor,i:ver10-iCursor"
 
 vim.opt.cursorline = true
+vim.opt.colorcolumn = "80"
+vim.opt.relativenumber = true
+vim.opt.number = true
+vim.opt.statuscolumn = "%s%=%{v:relnum?v:relnum:v:lnum} "
 
-vim.opt.nu = true
-vim.opt.rnu = true
+vim.opt.signcolumn = "yes:1"
 
 vim.opt.splitright = true
 vim.opt.splitbelow = true
@@ -16,6 +19,7 @@ vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.smartindent = true
 vim.opt.showmode = false
+vim.opt.wrap = false
 
 vim.opt.swapfile = false
 vim.opt.backup = false
@@ -32,10 +36,12 @@ vim.api.nvim_set_hl(0, "CursorReset", { bg = "#ffffff", fg = "#ffffff" })
 
 vim.api.nvim_create_autocmd("BufEnter", { pattern = "*", command = "setlocal formatoptions-=cro" })
 vim.api.nvim_create_autocmd("VimLeave", { pattern = "*", command = "set guicursor=a:ver100-CursorReset" })
-
-vim.api.nvim_create_autocmd("TextYankPost", {
-	pattern = "*",
-	command = "silent! lua vim.highlight.on_yank()",
-})
+vim.api.nvim_create_autocmd("TextYankPost", { pattern = "*", command = "silent! lua vim.highlight.on_yank()" })
+vim.api.nvim_create_autocmd("FileType", { pattern = "cpp", command = "set tabstop=4 shiftwidth=4" })
+vim.api.nvim_create_autocmd("FileType", { pattern = "cpp", command = "set tabstop=4 shiftwidth=4" })
+vim.api.nvim_create_autocmd(
+	"BufRead",
+	{ pattern = "*.vert,*.frag,", command = "set filetype=glsl tabstop=4 shiftwidth=4" }
+)
 
 vim.diagnostic.config({ underline = true, virtual_text = true })
--- .config/nvim/lua/alpan/plugins/colors.lua
+++ .config/nvim/lua/alpan/plugins/colors.lua
@@ -2,6 +2,9 @@ return {
 	"rose-pine/neovim",
 	priority = 1000,
 	config = function()
+		local dpurp = "#281e30"
+		local pwink = "#bebcd1"
+
 		require("rose-pine").setup({
 			variant = "moon",
 			dim_inactive_windows = false,
@@ -12,7 +15,16 @@ return {
 			highlight_groups = {
 				iCursor = { bg = "white" },
 				bCursor = { bg = "Muted" },
-				CursorLine = { bg = "#281e30" },
+				CursorLine = { bg = "none" },
+				CursorLineNr = { fg = "white" },
+				ColorColumn = { bg = dpurp },
+				SignColumn = { bg = dpurp },
+				GitSignsAdd = { bg = dpurp },
+				GitSignsChange = { bg = dpurp },
+				DiagnosticWarn = { bg = dpurp },
+				DiagnosticError = { bg = dpurp },
+				DiagnosticInfo = { bg = dpurp },
+				DiagnosticHint = { bg = dpurp },
 			},
 		})
 		vim.cmd.colorscheme("rose-pine-moon")
